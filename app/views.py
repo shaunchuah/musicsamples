1937a8b93ddbe3591e5d872339a8194e103184cf
Cher Shiong Chuah <chershiong@gmail.com>
2020-10-23 12:17:06 +0100

    added search and edit functions

--- app/views.py
+++ app/views.py
@@ -12,6 +12,7 @@ from django import template
 from .models import Sample
 from .forms import NewSampleForm, SampleForm
 from django.contrib import messages
+from django.db.models import Q
 
 @login_required(login_url="/login/")
 def index(request):
@@ -41,8 +42,6 @@ def pages(request):
         return HttpResponse(html_template.render(context, request))
 
 
-
-
 @login_required(login_url="/login/")
 def add(request):
     if request.method == "POST":
@@ -59,3 +58,38 @@ def add(request):
     else:
         form = SampleForm()
     return render(request, "add.html", {'form': form})
+
+@login_required(login_url="/login/")
+def sample_detail(request, pk):
+    sample = get_object_or_404(Sample, pk=pk)
+    return render(request, "sample-detail.html", {'sample': sample})
+    
+@login_required(login_url="/login/")
+def sample_edit(request,pk):
+    sample = get_object_or_404(Sample, pk=pk)
+    if request.method == "POST":
+        form = SampleForm(request.POST, instance=sample)
+        if form.is_valid():
+            sample = form.save(commit=False)
+            sample.last_modified_by = request.user.username
+            sample.save()
+            messages.success(request, 'Sample updated successfully.')
+            return redirect('/')
+    else:
+        form = SampleForm(instance=sample)
+    return render(request, 'sample-edit.html', {'form': form})
+
+def search(request):
+    query_string=''
+    found_samples = None
+    if ('q' in request.GET) and request.GET['q'].strip():
+        query_string = request.GET.get('q')
+        sample_list = Sample.objects.filter(
+            Q(musicsampleid__icontains=query_string)|
+            Q(patientid__icontains=query_string)|
+            Q(sample_location__icontains=query_string)|
+            Q(sample_type__icontains=query_string)|
+            Q(sample_comments__icontains=query_string))
+        return render(request, 'index.html', { 'query_string': query_string, 'sample_list': sample_list})
+    else:
+        return render(request, 'index.html', { 'query_string': 'Null' })
