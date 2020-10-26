27ac7ccba473cb3fb124ff96cdd3273e810c4e34
Cher Shiong Chuah <chershiong@gmail.com>
2020-10-26 11:26:36 +0000

    added csv exporting

--- app/urls.py
+++ app/urls.py
@@ -13,10 +13,12 @@ urlpatterns = [
 
     # The home page
     path('', views.index, name='home'),
+    path('analytics/', views.analytics, name='analytics'),
     path('add/', views.add, name='new_sample'),
     path('samples/<int:pk>/', views.sample_detail, name='sample_detail'),
     path('samples/<int:pk>/edit/', views.sample_edit, name='sample_edit'),
     path('samples/<int:pk>/checkout/', views.checkout, name='checkout'),
     path('search/', views.search, name="search"),
     path('bulkadd/', views.bulkadd, name='bulkadd'),
+    path('export_csv/', views.export_csv, name='export_csv'),
 ]
