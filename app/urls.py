b4ba438ce57c606597ca7238e5a60b002ab0819d
Cher Shiong Chuah <chershiong@gmail.com>
2020-11-13 12:38:50 +0000

    added export current view to excel

--- app/urls.py
+++ app/urls.py
@@ -25,5 +25,6 @@ urlpatterns = [
     path('search/', views.search, name="search"),
     path('bulkadd/', views.bulkadd, name='bulkadd'),
     path('export_csv/', views.export_csv, name='export_csv'),
-    path('export_excel/', views.export_excel_all, name='export_excel'),
+    path('export_excel_all/', views.export_excel_all, name='export_excel_all'),
+    path('export_excel/', views.export_excel, name='export_excel')
 ]
