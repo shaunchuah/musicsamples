4a19df4ec474e014908a4f50d12a5622d05f7678
Cher Shiong Chuah <chershiong@gmail.com>
2020-11-13 22:15:09 +0000

    added pie chart and dynamic colors

--- app/views.py
+++ app/views.py
@@ -38,9 +38,11 @@ def index(request):
 def analytics(request):
     total_samples = Sample.objects.all().filter(is_deleted=False).count()
     samples_by_month = Sample.objects.all().filter(is_deleted=False).annotate(sample_month=Trunc('sample_datetime', 'month')).values('sample_month').annotate(sample_count=Count('id')).order_by('sample_month')
+    samples_by_type = Sample.objects.all().filter(is_deleted=False).values('sample_type').annotate(sample_type_count=Count('id'))
     context = {
         'total_samples': total_samples,
-        'samples_by_month': samples_by_month
+        'samples_by_month': samples_by_month,
+        'samples_by_type': samples_by_type,
     }
     return render(request, "analytics.html", context)
 
