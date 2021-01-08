65fbbefd6f8b5fe1caa13cac85f03b6993897837
agtshm <chershiong@gmail.com>
2021-01-08 14:19:29 +0000

    linked samples to GID database

--- app/views.py
+++ app/views.py
@@ -131,11 +131,15 @@ def sample_detail(request, pk):
     sample_history = sample.history.filter(id=pk)
     changes = historical_changes(sample_history)
     first_change = sample_history.first()
+    if sample.patientid[0:3] == "GID":
+        gid_id = sample.patientid.split("-")[1]
+    else:
+        gid_id = None
     processing_time = None
     if sample.processing_datetime != None:
         time_difference = sample.processing_datetime - sample.sample_datetime
         processing_time = int(time_difference.total_seconds() / 60)
-    return render(request, "sample-detail.html", {'sample': sample, 'changes': changes, 'first': first_change, 'processing_time': processing_time})
+    return render(request, "sample-detail.html", {'sample': sample, 'changes': changes, 'first': first_change, 'processing_time': processing_time, 'gid_id': gid_id})
     
 @login_required(login_url="/login/")
 def sample_edit(request, pk):
