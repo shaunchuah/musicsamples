31dc2b4a29df332e5a2ed9521c54a95c098106fc
agtshm <chershiong@gmail.com>
2021-02-02 10:17:16 +0000

    successfully implemented manytomany tagging

--- core/urls.py
+++ core/urls.py
@@ -5,13 +5,14 @@ Copyright (c) 2019 - present AppSeed.us
 """
 
 from django.contrib import admin
-from django.urls import path, include  # add this
+from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", include("authentication.urls")),  # add this
-    path("", include("app.urls")),  # add this
+    path('', include("authentication.urls")),
+    path('', include("app.urls")),
     path('uploads/', include('ckeditor_uploader.urls')),
+    path('select2/', include('django_select2.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
