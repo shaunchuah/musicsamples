5b3f157bd1f275ffbf2753bfdb94c978f89fc45c
agtshm <chershiong@gmail.com>
2021-02-25 10:48:42 +0000

    implemented more testing

--- app/tests/test_urls.py
+++ app/tests/test_urls.py
@@ -6,64 +6,56 @@ from ..views import *
 class TestUrls(SimpleTestCase):
     def test_index_url_resolves(self):
         url = reverse('home')
-        self.assertEquals(resolve(url).func, index)
+        self.assertEqual(resolve(url).func, index)
 
     def test_analytics_url_resolves(self):
         url = reverse('analytics')
-        self.assertEquals(resolve(url).func, analytics)
+        self.assertEqual(resolve(url).func, analytics)
 
     def test_reference_url_resolves(self):
         url = reverse('reference')
-        self.assertEquals(resolve(url).func, reference)
+        self.assertEqual(resolve(url).func, reference)
 
     def test_archive_url_resolves(self):
         url = reverse('archive')
-        self.assertEquals(resolve(url).func, archive)
+        self.assertEqual(resolve(url).func, archive)
 
     def test_add_sample_url_resolves(self):
         url = reverse('new_sample')
-        self.assertEquals(resolve(url).func, add)
+        self.assertEqual(resolve(url).func, add)
 
     def test_view_single_sample_url_resolves(self):
         url = reverse('sample_detail', kwargs={'pk':1})
-        self.assertEquals(resolve(url).func, sample_detail)
+        self.assertEqual(resolve(url).func, sample_detail)
 
     def test_edit_sample_url_resolves(self):
         url = reverse('sample_edit', kwargs={'pk':23})
-        self.assertEquals(resolve(url).func, sample_edit)
+        self.assertEqual(resolve(url).func, sample_edit)
 
     def test_delete_sample_url_resolves(self):
         url = reverse('delete', kwargs={'pk':3})
-        self.assertEquals(resolve(url).func, delete)
+        self.assertEqual(resolve(url).func, delete)
 
     def test_restore_sample_url_resolves(self):
         url = reverse('restore', kwargs={'pk':3})
-        self.assertEquals(resolve(url).func, restore)
+        self.assertEqual(resolve(url).func, restore)
 
     def test_checkout_sample_url_resolves(self):
         url = reverse('checkout', kwargs={'pk':3})
-        self.assertEquals(resolve(url).func, checkout)
+        self.assertEqual(resolve(url).func, checkout)
 
     def test_search_url_resolves(self):
         url = reverse('search')
-        self.assertEquals(resolve(url).func, search)
+        self.assertEqual(resolve(url).func, search)
 
     def test_csv_export_url_resolves(self):
         url = reverse('export_csv')
-        self.assertEquals(resolve(url).func, export_csv)
+        self.assertEqual(resolve(url).func, export_csv)
 
     def test_export_excel_url_resolves(self):
         url = reverse('export_excel')
-        self.assertEquals(resolve(url).func, export_excel)
+        self.assertEqual(resolve(url).func, export_excel)
 
     def test_account_page_url_resolves(self):
         url = reverse('account')
-        self.assertEquals(resolve(url).func, account)
-
-    def test_qrcode_update_location_api_call(self):
-        response = self.client.get('/api/samples/')
-        self.assertEqual(response.status_code, 200)
-    
-    def test_qrcode_mark_samples_as_used_api_call(self):
-        response = self.client.get('/api/samples_used/')
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(resolve(url).func, account)
