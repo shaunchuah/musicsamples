d7efc4f0fa818a6ca5f256427c0b212b9809fd3b
agtshm <chershiong@gmail.com>
2021-02-25 00:56:38 +0000

    added testing framework

--- app/tests/test_models.py
+++ app/tests/test_models.py
@@ -1,2 +1,11 @@
 from django.test import TestCase, SimpleTestCase, Client
+from django.db import models
+from mixer.backend.django import mixer
+import pytest
 
+pytestmark = pytest.mark.django_db
+
+class TestSample:
+    def test_model(self):
+        obj = mixer.blend('app.Sample')
+        assert obj.pk == 1, 'Should create sample instance with primary key id of 1.'
