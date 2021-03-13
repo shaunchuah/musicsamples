76fa723db437d95dfb447c9032a8f8a713419099
Shaun Chuah <chershiong@gmail.com>
2021-03-13 15:44:21 +0000

    updated sample add form

--- app/tests/test_forms.py
+++ app/tests/test_forms.py
@@ -10,19 +10,43 @@ class TestSampleForm:
         assert form.is_valid() is False, 'Form should be invalid if no data provided'
 
 
-sample_data = {
-    'musicsampleid': 'sample001',
-    'patientid': 'patient001',
-    'sample_location': 'location1',
-    'sample_type': 'PaxGene ccfDNA plasma child aliquot',
-    'sample_datetime': '2021-01-01T11:00',
-    'sample_comments': '',
-    'processing_datetime': '',
-    'sample_sublocation': '',
-    'sample_volume': '',
-    'sample_volume_units': '',
-    'freeze_thaw_count': '',
-    'haemolysis_reference': '',
-    'biopsy_location': '',
-    'biopsy_inflamed_status': ''
-}
+def test_sample_add_form():
+    sample_data = {
+        'musicsampleid': 'test001',
+        'patientid': 'patient001',
+        'sample_location': 'location001',
+        'sample_type': 'test_sample_type',
+        'sample_datetime': '2020-01-01T13:20:30',
+        'sample_comments': '',
+        'processing_datetime': '2020-01-01T13:20:30',
+        'sample_sublocation': '',
+        'sample_volume': '',
+        'sample_volume_units': '',
+        'freeze_thaw_count': 0,
+        'haemolysis_reference': '',
+        'biopsy_location': '',
+        'biopsy_inflamed_status': ''
+    }
+    form = forms.SampleForm(data=sample_data)
+    assert form.is_valid() is True
+
+
+def test_sample_add_form_without_processing_time():
+    sample_data = {
+        'musicsampleid': 'test001',
+        'patientid': 'patient001',
+        'sample_location': 'location001',
+        'sample_type': 'test_sample_type',
+        'sample_datetime': '2020-01-01T13:20:30',
+        'sample_comments': '',
+        'processing_datetime': '',
+        'sample_sublocation': '',
+        'sample_volume': '',
+        'sample_volume_units': '',
+        'freeze_thaw_count': 0,
+        'haemolysis_reference': '',
+        'biopsy_location': '',
+        'biopsy_inflamed_status': ''
+    }
+    form = forms.SampleForm(data=sample_data)
+    assert form.is_valid() is True
