3b71a6087308eb878a2df83898a8b82686a9a1b7
agtshm <chershiong@gmail.com>
2021-03-18 21:23:25 +0000

    added multiple sample tagging page

--- app/serializers.py
+++ app/serializers.py
@@ -21,3 +21,25 @@ class SampleIsFullyUsedSerializer(serializers.HyperlinkedModelSerializer):
             'is_fully_used',
         ]
         lookup_field = 'musicsampleid'
+
+
+class MultipleSampleSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Sample
+        fields = [
+            'musicsampleid',
+            'sample_location',
+            'sample_sublocation',
+            'patientid',
+            'sample_type',
+            'haemolysis_reference',
+            'biopsy_location',
+            'biopsy_inflamed_status',
+            'sample_datetime',
+            'processing_datetime',
+            'sample_comments',
+            'sample_volume',
+            'sample_volume_units',
+            'freeze_thaw_count',
+        ]
+        lookup_field = 'musicsampleid'
