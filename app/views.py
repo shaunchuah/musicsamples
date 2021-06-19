48d83587ab6ee02a021b04b99a42ac2c0a03c076
agtshm <chershiong@gmail.com>
2021-06-19 18:11:50 +0100

    fix: spacing

--- app/views.py
+++ app/views.py
@@ -78,7 +78,7 @@ def gid_overview(request):
         sample_list = Sample.objects.filter(is_deleted=False).filter(is_fully_used=False).filter(sample_type=query).order_by("patientid")
     else:
         sample_list = Sample.objects.none()
-    context = {'sample_list': sample_list, 'sample_category_list': sample_category_list, 'query':query}
+    context = {'sample_list': sample_list, 'sample_category_list': sample_category_list, 'query': query}
     return render(request, "gid_overview.html", context)
 
 
