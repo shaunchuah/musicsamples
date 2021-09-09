b0f6ed65393dec57f388a30b14a6072a966e1718
agtshm <chershiong@gmail.com>
2021-09-09 21:51:12 +0100

    fix: patient ID now saves as uppercase

--- app/models.py
+++ app/models.py
@@ -35,6 +35,7 @@ class Sample(models.Model):
 
     def clean(self):
         self.musicsampleid = self.musicsampleid.upper()
+        self.patientid = self.patientid.upper()
 
     def __str__(self):
         return self.musicsampleid
