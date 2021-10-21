44b87a70069082cca5ae20506e7940bb722124b8
agtshm <chershiong@gmail.com>
2021-10-21 22:15:20 +0100

    fix: clean up advanced filtering page

---
+++ app/choices.py
@@ -0,0 +1,94 @@
+SAMPLE_TYPE_CHOICES = (
+    ("", "Select type"),
+    (
+        "Standard EDTA tube",
+        (
+            ("Standard EDTA tube", "Standard EDTA tube"),
+            ("EDTA plasma child aliquot", "EDTA plasma child aliquot"),
+        ),
+    ),
+    (
+        "PaxGene ccfDNA tube",
+        (
+            ("PaxGene ccfDNA tube", "PaxGene ccfDNA tube"),
+            (
+                "PaxGene ccfDNA plasma child aliquot",
+                "PaxGene ccfDNA plasma child aliquot",
+            ),
+            (
+                "PaxGene ccfDNA extracted cfDNA",
+                "PaxGene ccfDNA extracted cfDNA",
+            ),
+        ),
+    ),
+    (
+        "PaxGene RNA tube",
+        (
+            ("PaxGene RNA tube", "PaxGene RNA tube"),
+            ("PaxGene RNA child aliquot", "PaxGene RNA child aliquot"),
+        ),
+    ),
+    (
+        "Standard Gel/Serum tube",
+        (
+            ("Standard gel tube", "Standard gel tube"),
+            ("Serum child aliquot", "Serum child aliquot"),
+        ),
+    ),
+    (
+        "Tissue/Biopsy",
+        (
+            ("Formalin biopsy", "Formalin biopsy"),
+            ("RNAlater biopsy", "RNAlater biopsy"),
+            ("Paraffin tissue block", "Paraffin tissue block"),
+        ),
+    ),
+    (
+        "Stool",
+        (
+            ("Calprotectin", "Calprotectin"),
+            ("FIT", "FIT"),
+            ("OmniGut", "Omnigut"),
+            ("Stool supernatant", "Stool supernatant"),
+        ),
+    ),
+    ("Saliva", (("Saliva", "Saliva"),)),
+    ("Other", (("Other", "Other please specify in comments"),)),
+)
+
+HAEMOLYSIS_REFERENCE_CHOICES = (
+    ("", "Select category"),
+    ("0", "Minimal"),
+    ("20", "20 mg/dL"),
+    ("50", "50 mg/dL"),
+    ("100", "100 mg/dL (unusable)"),
+    ("250", "250 mg/dL (unusable)"),
+    ("500", "500 mg/dL (unusable)"),
+    ("1000", "1000 mg/dL (unusable)"),
+)
+
+BIOPSY_LOCATION_CHOICES = (
+    ("", "Select biopsy location"),
+    ("Terminal ileum", "Terminal ileum"),
+    ("Caecum", "Caecum"),
+    ("Ascending colon", "Ascending colon"),
+    ("Transverse colon", "Transverse colon"),
+    ("Descending colon", "Descending colon"),
+    ("Sigmoid colon", "Sigmoid colon"),
+    ("Rectum", "Rectum"),
+    ("Right colon", "Right colon"),
+    ("Left colon", "Left colon"),
+)
+
+BIOPSY_INFLAMED_STATUS_CHOICES = (
+    ("", "Select inflamed status"),
+    ("inflamed", "Inflamed"),
+    ("uninflamed", "Uninflamed"),
+    ("healthy", "Healthy"),
+)
+
+SAMPLE_VOLUME_UNIT_CHOICES = (
+    ("", "Select unit"),
+    ("ml", "ml"),
+    ("ul", "ul"),
+)
--- app/filters.py
+++ app/filters.py
@@ -1,13 +1,25 @@
 import django_filters
 
 from .models import Sample
+from .choices import SAMPLE_TYPE_CHOICES
 
 
 class SampleFilter(django_filters.FilterSet):
-    patient_id = django_filters.CharFilter(lookup_expr="iexact")
-    sample_location = django_filters.CharFilter(lookup_expr="iexact")
-    sample_sublocation = django_filters.CharFilter(lookup_expr="iexact")
-    sample_type = django_filters.CharFilter(lookup_expr="icontains")
+    patient_id = django_filters.AllValuesFilter(label="Patient ID")
+    sample_location = django_filters.AllValuesFilter(label="Sample Location")
+    sample_sublocation = django_filters.AllValuesFilter(label="Sample Sublocation")
+    sample_type = django_filters.ChoiceFilter(
+        label="Sample Type", choices=SAMPLE_TYPE_CHOICES
+    )
+    is_fully_used = django_filters.BooleanFilter(label="Used Samples?")
+    sample_datetime = django_filters.DateFromToRangeFilter(
+        widget=django_filters.widgets.RangeWidget(attrs={"type": "date"}),
+        label="Sample Date Range",
+    )
+    sample_comments = django_filters.CharFilter(
+        lookup_expr="icontains", label="Sample Comments Contains"
+    )
+    sample_volume = django_filters.NumberFilter(label="Sample Volume")
 
     class Meta:
         model = Sample
@@ -16,6 +28,8 @@ class SampleFilter(django_filters.FilterSet):
             "sample_location",
             "sample_sublocation",
             "sample_type",
-            "is_deleted",
             "is_fully_used",
+            "sample_datetime",
+            "sample_comments",
+            "sample_volume",
         ]
