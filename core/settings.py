df421d483d58da3740895efb8a4fe2311ace4b8c
Shaun Chuah <chershiong@gmail.com>
2022-02-01 17:50:56 +0000

    update packages and remove jazzmin

--- core/settings.py
+++ core/settings.py
@@ -35,7 +35,6 @@ ALLOWED_HOSTS = config("ALLOWED_HOSTS", cast=Csv())
 # Application definition
 
 INSTALLED_APPS = [
-    "jazzmin",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
