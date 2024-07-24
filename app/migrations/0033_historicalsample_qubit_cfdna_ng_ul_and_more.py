b85aa03c977dcb8d79f25faef338b3d127b6eebf
Shaun Chuah <chershiong@gmail.com>
2024-07-24 17:35:30 +0100

    ruff format migration file

--- app/migrations/0033_historicalsample_qubit_cfdna_ng_ul_and_more.py
+++ app/migrations/0033_historicalsample_qubit_cfdna_ng_ul_and_more.py
@@ -4,7 +4,6 @@ from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
         ("app", "0032_alter_historicalsample_music_timepoint_and_more"),
     ]
