92f7d6ae59c673dcd5d58c01be4a9feda953f2ec
Shaun Chuah <chershiong@gmail.com>
2024-07-24 11:20:47 +0100

    add drf test for invalid response

--- app/tests/test_serializers.py
+++ app/tests/test_serializers.py
@@ -13,6 +13,31 @@ class MultipleSamplesTest(APITestCase):
         self.client = APIClient()
         self.client.force_authenticate(user=self.user)
 
+    def test_invalid_response_without_study_name(self):
+        sample_data = {
+            "study_name": "",
+            "sample_id": "test001",
+            "patient_id": "patient001",
+            "sample_location": "location001",
+            "sample_type": "test_sample_type",
+            "sample_datetime": "2020-01-01T13:20:30",
+            "sample_comments": "",
+            "processing_datetime": "2020-01-01T13:20:30",
+            "sample_sublocation": "",
+            "sample_volume": "",
+            "sample_volume_units": "",
+            "freeze_thaw_count": 0,
+            "haemolysis_reference": "",
+            "biopsy_location": "",
+            "biopsy_inflamed_status": "",
+        }
+        response = self.client.post(
+            "/api/multiple_samples/", sample_data, format="json"
+        )
+        print(response.data)
+        assert response.status_code == 400  # bad request
+        assert response.data["study_name"][0].code == "invalid_choice"
+
     def test_patient_id_uppercase(self):
         sample_data = {
             "study_name": "gidamps",
@@ -34,6 +59,5 @@ class MultipleSamplesTest(APITestCase):
         response = self.client.post(
             "/api/multiple_samples/", sample_data, format="json"
         )
-        print(response.data)
         assert response.status_code == 201
         assert response.data["patient_id"] == "PATIENT001"
