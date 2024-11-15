ee55c08ccc2a6079184a8c47f48628cb1bc1c266
Shaun Chuah <chershiong@gmail.com>
2024-11-15 15:07:05 +0000

    Update factory attributes to use choices values and upgrade dependencies

--- app/factories.py
+++ app/factories.py
@@ -14,11 +14,11 @@ class SampleFactory(DjangoModelFactory):
     class Meta:
         model = Sample
 
-    study_name = LazyAttribute(lambda x: choice(StudyNameChoices.choices))
+    study_name = LazyAttribute(lambda x: choice(StudyNameChoices.values))
     sample_id = Sequence(lambda n: "DEMO-%05d" % n)
     patient_id = Sequence(lambda n: "DEMO-%d" % n)
     sample_location = LazyAttribute(lambda x: choice(LOCATION_CHOICES))
-    sample_type = LazyAttribute(lambda x: choice(SampleTypeChoices.choices))
+    sample_type = LazyAttribute(lambda x: choice(SampleTypeChoices.values))
     sample_datetime = Faker("date_time_this_year", tzinfo=timezone.utc)
     created_by = Faker("email")
     last_modified_by = Faker("email")
