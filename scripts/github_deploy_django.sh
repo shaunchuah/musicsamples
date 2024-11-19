a895c723c4fd0d193c81e729f3d443a45fa788e2
Shaun Chuah <chershiong@gmail.com>
2024-11-19 15:51:36 +0000

    prevent auto deployment of this change as will need manual migration

--- scripts/github_deploy_django.sh
+++ scripts/github_deploy_django.sh
@@ -10,8 +10,8 @@ echo "Activating python virtual environment..."
 source ~/app/env/bin/activate
 echo "Installing requirements.txt..."
 pip install -r requirements.txt
-echo "Running migrations..."
-python manage.py migrate
+# echo "Running migrations..."
+# python manage.py migrate
 echo "Collecting static files..."
 python manage.py collectstatic --noinput;
 echo "Completed django deployment."
