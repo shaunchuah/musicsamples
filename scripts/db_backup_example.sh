62985137fab631c8f7ab26b89b47446860b20bc7
Shaun Chuah <chershiong@gmail.com>
2024-12-03 12:21:53 +0000

    feat: update R documentation for datasets

--- scripts/db_backup_example.sh
+++ scripts/db_backup_example.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-cd ~/music_samples
+cd ~/musicsamples
 cp production.sqlite3 ~/db_backups/$(date +%Y-%m-%d_%H-%M-%S)_musicsamples_db.sqlite3
 aws s3 sync ~/db_backups s3://musicsamplesdbbackup
